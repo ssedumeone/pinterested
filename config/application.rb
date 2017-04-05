2154dcd2d98dc2ceebc67732671d983187cf57a6
Ronald Muwonge <ssedume@gmail.com>
2017-04-05 17:49:40 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,5 @@
 require_relative 'boot'
-
 require 'rails/all'
-require 'devise' 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
