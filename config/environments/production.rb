62b82370f1775704f770594ef506edc802a868b3
Ronald Muwonge <ssedume@gmail.com>
2017-04-17 11:59:27 +0300

    integrated AmazonS3 with Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-  :bucket => ENV['AWS_BUCKET'],
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-}
-}
-end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -92,4 +84,12 @@ end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 config.action_mailer.default_url_options = { host: ' pinterested1978.herokuapp.com' }
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+  :bucket => ENV['AWS_BUCKET'],
+  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+ }
+}
 end
