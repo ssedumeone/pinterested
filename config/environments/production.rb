90f61811a9bab9267c599ff8f056fbadebdda1b1
Ronald Muwonge <ssedume@gmail.com>
2017-04-17 12:53:07 +0300

    integrated AmazonS3 with Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,8 @@ config.paperclip_defaults = {
   :bucket => ENV['AWS_BUCKET'],
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+
  }
+ :s3_region => ENV['AWS_REGION'],
 }
 end
