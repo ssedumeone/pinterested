f7c32843739297e4ba34a599e08a2824d64c078f
Ronald Muwonge <ssedume@gmail.com>
2017-04-21 12:55:13 +0300

    added imagemagick and paperclip, updated pins views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,12 +82,13 @@ Rails.application.configure do
   end
   # config/environments/production.rb
 config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-    :bucket =>ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_region =>ENV['AWS_REGION'],
+  :storage => :s3,
+  :s3_host_name => 'us-east-2.amazonaws.com',
+  :s3_credentials => {
+  :bucket =>ENV['S3_BUCKET_NAME'],
+  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+:s3_region =>ENV['AWS_REGION'],
   }
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
+      devise_scope :user do  get '/users/sign_out' => 'devise/sessions#destroy' end
   root 'home#index'
   get  'home/about'
-  
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
